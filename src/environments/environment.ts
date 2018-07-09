37ac650ed1dd89f81f31a564441e4f8b9fc76ff9
charliecortez2018 <41024751+charliecortez2018@users.noreply.github.com>
2018-07-09 15:06:36 -0500

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCEUGaSL1glxYngagMszqS5rvoXynyRoA0",
+    authDomain: "camino-de-vida-2382d.firebaseapp.com",
+    databaseURL: "https://camino-de-vida-2382d.firebaseio.com",
+    projectId: "camino-de-vida-2382d",
+    storageBucket: "camino-de-vida-2382d.appspot.com",
+    messagingSenderId: "780500669281"
+  }
+};
